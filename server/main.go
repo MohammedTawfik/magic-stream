6ace549cdafe29cb5a02b82e15589f6ec98f6a1b
Mohammed Tawfik <Mohamed.Tawfik2@bd.com>
2025-10-01 20:49:52 +0300

    Create simple web server using gin-gonic package

---
+++ server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Hello, World!"})
+	})
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
